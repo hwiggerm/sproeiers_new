9bf37a481edd59681e631175520eb98723533871
Hans <hwiggerm@gmail.com>
2021-03-19 15:31:07 +0100

    .

--- ss.py
+++ ss.py
@@ -1,28 +1,22 @@
 from library.valves import ctrlvalves
-from library.core import logger.py
+from library.core import logger
 
 import time
 
 if ctrlvalves.openvalve('http://10.0.0.141/','tuinon'):
-        logger.writeline('Valve Switched')
+        logger.writeline('Valve Switched tuin')
 else:
-	print('Error in switching')
+	logger.writeline('Error in switching')
+
 if ctrlvalves.openvalve('http://10.0.0.141/','zwembadon'):
-        print('Valve Switched')
+	logger.writeline('Valve Switched zwembad')
 else:
-        print('Error in switching')
+        logger.writeline('Error in switching')
 
 time.sleep(5)
 
 if ctrlvalves.openvalve('http://10.0.0.141/','poweroff'):
-        print('poweroff')
+        logger.writeline('poweroff')
 else:
-        print('Error in switching')
-
-
-
-
-
-
-
+        logger.writeline('Error in switching')
 
