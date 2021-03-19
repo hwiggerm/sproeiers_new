2a4ef9e42f5ebb409b80355401d71f3d0594044b
Hans <hwiggerm@gmail.com>
2021-03-19 12:58:03 +0100

    updated libs valves

--- ss.py
+++ ss.py
@@ -1,18 +1,25 @@
-import  valves
+from library.valves import ctrlvalves
+
 import time
 
-if valves.openvalve('http://10.0.0.141/','tuinon'):
+if ctrlvalves.openvalve('http://10.0.0.141/','tuinon'):
 	print('Valve Switched')
 else:
 	print('Error in switching')
-
-
-time.sleep(5)
-
-if valves.openvalve('http://10.0.0.141/','zwembadon'):
+if ctrlvalves.openvalve('http://10.0.0.141/','zwembadon'):
         print('Valve Switched')
 else:
         print('Error in switching')
+
+time.sleep(5)
+
+if ctrlvalves.openvalve('http://10.0.0.141/','poweroff'):
+        print('poweroff')
+else:
+        print('Error in switching')
+
+
+
 
 
 
