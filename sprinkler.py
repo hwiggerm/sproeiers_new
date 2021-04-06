cabe43821fb1e91da5749667980f62b3566f0392
Hans Wiggerman <hwiggerm@gmail.com>
2021-04-06 21:12:18 +0200

    .

--- sprinkler.py
+++ sprinkler.py
@@ -3,7 +3,7 @@ import os
 import sys
 import urllib.request
 import time
-
+from library.valves import ctrlvalves
 
 from library.sensors import getdht
 from library.sensors import getowmweather
@@ -13,37 +13,34 @@ klepstatus = 'status'
 zwemklep = 'zwembadon'
 tuinklep = 'tuinon'
 
-def connect(host):
-    try:
-        reply = 0
-        fb = urllib.request.urlopen(host)
-        reply = fb.getcode()
-        return reply
+# def connect(host):
+#     try:
+#         reply = 0
+#         fb = urllib.request.urlopen(host)
+#         reply = fb.getcode()
+#         return reply
  
-    except:
-        return 404
+#     except:
+#         return 404
+
+# def openvalve(sproeiklep,klepstatus):
+#     # first check if the swithc can be reached
+#     alive = connect(sproeiklep) 
+#     if alive == 404:
+#         print('Connect Error')
+#         return(false)
+#     else:
+#         print('Connected and feeling fine, status ' + str(alive))
+#         time.sleep(5)
+
+#     print('set ' + sproeiklep )
+#     valve = connect(sproeiklep+klepstatus)
+#     print ("valve status :", str(valve))
+#     time.sleep(5)
+#     return(valve)
 
 
-
-
-def openvalve(sproeiklep,klepstatus):
-    # first check if the swithc can be reached
-    alive = connect(sproeiklep) 
-    if alive == 404:
-        print('Connect Error')
-        return(false)
-    else:
-        print('Connected and feeling fine, status ' + str(alive))
-        time.sleep(5)
-
-    print('set ' + sproeiklep )
-    valve = connect(sproeiklep+klepstatus)
-    print ("valve status :", str(valve))
-    time.sleep(5)
-    return(valve)
-
-
-print(connect(sproeiklep))
-print(openvalve(sproeiklep,tuinklep))
+print(ctrlvalves.connect(sproeiklep))
+print(ctrlvalves.openvalve(sproeiklep,tuinklep))
 time.sleep(5)
-print(openvalve(sproeiklep,zwemklep))
+print(ctrlvalves.openvalve(sproeiklep,zwemklep))
