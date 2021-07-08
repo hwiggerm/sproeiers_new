0aeeb4633189af0de1961045dff66b6b422d9074
Hans Wiggerman <hwiggerm@gmail.com>
2021-07-08 14:56:42 +0200

    Update ctrlpump.py
    added more pump control methods

--- library/valves/ctrlpump.py
+++ library/valves/ctrlpump.py
@@ -1,5 +1,12 @@
 import RPi.GPIO as GPIO
+import os
+from library.valves import ctrlvalves
+
 in1 = 16
+
+
+klepsysteem = os.environ.get('KLEPSYSTEEM')
+
 
 def portinit():
     GPIO.setmode(GPIO.BOARD)
@@ -12,3 +19,36 @@ def pumpon():
 def pumpoff():
     GPIO.output(in1, False)
 
+
+def sproeituin():
+    if ctrlvalves.openvalve(klepsysteem,'tuinon'):
+        return('Proei  tuin')
+    else:
+        return('Error in switching tuin')
+
+def sproeizwembad():
+    if ctrlvalves.openvalve(klepsysteem,'zwembadon'):
+        return('Sproei zwembad')
+    else:
+        return('Error in switching zwembad')
+
+def startsproeier():
+    if ctrlvalves.openvalve(klepsysteem,'tuinon'):
+        pumpon()
+        return('Sproei tuin')
+    else:
+        return('Pomp niet aan error is kleppen')
+
+def stopsproeier():
+    pumpoff()
+
+    if ctrlvalves.openvalve(klepsysteem,'poweroff'):
+        return('Run Poweroff')
+    else:
+        #error in switching
+        time.sleep(10)
+        #try again 
+        if ctrlvalves.openvalve(klepsysteem,'poweroff'):
+            return('Poweroff completed') 
+        else:
+            return('Error in poweroff kleppen')
